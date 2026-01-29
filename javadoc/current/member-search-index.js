be51188916ac95639aecadeba3175edc0863d0c4
GitHub Action <action@github.com>
2026-01-29 17:15:45 +0000

    chore: updates index.html files incl. javadoc versions

--- javadoc/current/member-search-index.js
+++ javadoc/current/member-search-index.js
@@ -1,1 +1,1 @@
-memberSearchIndex = [{"p":"dev.thriving.oss.java.library.template","c":"JavaLibraryTemplate","l":"JavaLibraryTemplate()","u":"%3Cinit%3E()"},{"p":"dev.thriving.oss.java.library.template","c":"JavaLibraryTemplate","l":"toFunkyCase(String)","u":"toFunkyCase(java.lang.String)"}];updateSearchResults();
+memberSearchIndex = [{"p":"tech.ixor.calclox","c":"JavaLibraryTemplate","l":"JavaLibraryTemplate()","u":"%3Cinit%3E()"},{"p":"tech.ixor.calclox","c":"JavaLibraryTemplate","l":"toFunkyCase(String)","u":"toFunkyCase(java.lang.String)"}];updateSearchResults();
